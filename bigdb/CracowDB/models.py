02e40163c022ba109e0b85c7304dabc4c09ef37f
Marcin <marcpostbox@gmail.com>
2018-11-19 23:29:58 +0100

    kolejne wgranie plikow

--- bigdb/CracowDB/models.py
+++ bigdb/CracowDB/models.py
@@ -4,18 +4,36 @@ from django.db import models
 
 
 class kwdata(models.Model):
-	prefix  = models.CharField(max_length=4)
-	number  = models.CharField(max_length=4)
-	correct = models.CharField(max_length=1)
+	kw_number  = models.CharField(max_length=15, default='', help_text='pełny numer kw')
+	date_of_save = models.DateField(null=True, blank=True)
+	date_of_close = models.DateField(null=True, blank=True)
 
 	KW_NIER_GR = 'NG'
 	KW_LOK_MIESZ = 'LM'
 	KW_TYPE_CHOICES = (
 		(KW_NIER_GR, 'Nieruchomość gruntowa'),
-		(KW_LOK_MIESZ, 'Lokal mieszkalny'),
+		(KW_LOK_MIESZ, 'Lokal stanowiący odrębną nieruchomość'),
 	)
 	kw_type = models.CharField(max_length=2, choices=KW_TYPE_CHOICES)
 
+
 	def __str__(self):
-		return self.title
+		return self.kw_number
+
+	@property
+	def get_date_of_save(self):
+		if self.date_of_save is None:
+			return ''
+		else:
+			return self.date_of_save
+
+
+	@property
+	def get_kw_type(self):
+		if self.kw_type==self.KW_NIER_GR:
+			return 'Nieruchomość gruntowa'
+		else:
+			return 'Lokal stanowiący odrębną nieruchomość'
+	
+
 		
--- bigdb/CracowDB/views.py
+++ bigdb/CracowDB/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import kwdata
 
 # Create your views here.
+
+def kw_list(request):
+	kwrecs = kwdata.objects.all()
+	return render(request, 'CracowDB/kw_list.html', {'kwrecs':kwrecs})
